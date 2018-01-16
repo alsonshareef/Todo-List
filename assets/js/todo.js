69d7612756d971656637faa5165ac5a9719104c7
Alson Shareef <alson.shareef@gmail.com>
2018-01-16 16:50:33 +0800

    Finished off jQuery functionality

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,14 +1,24 @@
 // Turn todos grey by clicking on them
-$("li").click(function(){
-	// if 'li' is grey:
-	if ($(this).css("color") === "rgb(128, 128, 128)") {
-		// turn black
-		$(this).css("color", "rgb(0, 0, 0)");
-		$(this).css("text-decoration", "none")
-	} 
-	// else, turn it grey
-	else {
-		$(this).css("color", "rgb(128, 128, 128)");
-		$(this).css("text-decoration", "line-through");
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+// Click on 'X' to delete todo
+$('ul').on("click", "span", function (event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+// Type todo into text box and add to ul
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13) {
+		// set input text to a variable
+			var todoText = ($(this).val());
+		// remove input text from text box
+			$(this).val(null);
+		// add todoText as an li to the ul
+			$("ul").append("<li><span>X</span> " + todoText + "</li>");
 	}
 });
